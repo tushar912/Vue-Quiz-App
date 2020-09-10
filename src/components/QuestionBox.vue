7e4188323db561d20d137a607fdde305f6325a5b
Tushar912 <tushar.912u@gmail.com>
2020-09-10 19:15:00 +0530

    added styles

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,21 @@
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.list-group-item:hover {
+  background: #EEE;
+  cursor: pointer;
+}
+.btn {
+  margin: 0 5px;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: red;
+}
+</style>
